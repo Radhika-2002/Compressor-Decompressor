9710d7e86be18f479e48c1e898066cfc42720740
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-02-22 00:10:19 +0530

    first commit

--- src/main/java/comp_decomp/compressor.java
+++ src/main/java/comp_decomp/compressor.java
@@ -1,2 +1,32 @@
-package comp_decomp;public class compressor {
+package comp_decomp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+public class compressor {
+    public static void method (File file) throws IOException{
+        String fileDirectory = file.getParent();
+
+        FileInputStream fis = new FileInputStream(file);
+        FileOutputStream fos = new FileOutputStream(fileDirectory+"/CompressedFile.gz");
+        GZIPOutputStream gzip = new GZIPOutputStream(fos);
+
+        byte[] buffer = new byte[1024];
+        int len;
+        while((len= fis.read(buffer)) != -1){
+            gzip.write(buffer,0, len);
+        }
+        gzip.close();
+        fis.close();
+        fos.close();
+
+    }
+    public static void main(String[] args) throws IOException {
+        File path = new File("C:\\Users\\tkin\\Downloads\\Compressor\\compressor_decompressor\\TestFile");
+        method(path);
+
+    }
 }
--- src/main/java/org/example/AppFrame.java
+++ src/main/java/org/example/AppFrame.java
@@ -1,1 +1,63 @@
 package org.example;
+
+import comp_decomp.compressor;
+import comp_decomp.decompressor;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class AppFrame extends JFrame implements ActionListener {
+    JButton compressButton;
+    JButton decompressButton;
+
+    public AppFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout()); // Use a layout manager to arrange components
+
+        compressButton = new JButton("Select file to compress");
+        compressButton.addActionListener(this);
+        this.add(compressButton);
+
+        decompressButton = new JButton("Select file to decompress");
+        decompressButton.addActionListener(this);
+        this.add(decompressButton);
+
+        this.setSize(500, 200); // Adjust the size as needed
+        this.getContentPane().setBackground(Color.BLUE);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Action performed logic here
+        if(e.getSource()==compressButton){
+            JFileChooser fileChooser = new JFileChooser();
+            int response = fileChooser.showSaveDialog(null);
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file = new File(String.valueOf(fileChooser.getSelectedFile().getAbsoluteFile()));
+                System.out.println(file);
+                try{
+                    compressor.method(file);
+                }catch (Exception ee){
+                    JOptionPane.showMessageDialog(null,ee.toString());
+                }
+            }
+        }
+        if(e.getSource()==decompressButton){
+            JFileChooser fileChooser = new JFileChooser();
+            int response = fileChooser.showSaveDialog(null);
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file = new File(String.valueOf(fileChooser.getSelectedFile().getAbsoluteFile()));
+                System.out.println(file);
+                try{
+                    decompressor.method(file);
+                }catch (Exception ee){
+                    JOptionPane.showMessageDialog(null,ee.toString());
+                }
+            }
+        }
+    }
+}
